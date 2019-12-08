4dc2840649b6e3acb77a523f40fc38867767fb51
JeeErn <jee.ern88@gmail.com>
2019-12-08 13:57:29 -0800

    hard coded obstacle 1

--- ros_workspaces/ourProj/src/our_pkg/src/obstacle_create_client.py
+++ ros_workspaces/ourProj/src/our_pkg/src/obstacle_create_client.py
@@ -2,20 +2,21 @@ import sys
 import rospy
 from our_pkg.srv import obstacle_create_srv
 
-def obstacle_create_client(pa):
+def obstacle_create_client():
     rospy.wait_for_service('create_obstacle')
     try:
         create_obstacle = rospy.ServiceProxy('create_obstacle', obstacle_create_srv)
-        resp = create_obstacle(pa[0],pa[1],pa[2],pa[3],pa[4],pa[5])
+        # resp = create_obstacle(pa[0],pa[1],pa[2],pa[3],pa[4],pa[5])
+        resp = create_obstacle("obs1", 0, 5, 30, 10, 10000)
         return resp
     except:
         print("Service call failed")
     
 if __name__ == "__main__":
-    if len(sys.argv) == 8:
-        para = sys.argv[3:]
-        para = [float(i) for i in para]
-        para.insert(0, sys.argv[2])
-    else:
-        sys.exit(1)
-    a = obstacle_create_client(para)
+    # if len(sys.argv) == 8:
+    #     para = sys.argv[3:]
+    #     para = [float(i) for i in para]
+    #     para.insert(0, sys.argv[2])
+    # else:
+    #     sys.exit(1)
+    a = obstacle_create_client()
