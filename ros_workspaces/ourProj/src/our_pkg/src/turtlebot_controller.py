6fa65c91eee18bdd9417a2a780524e2207e3a7f2
JeeErn <jee.ern88@gmail.com>
2019-12-11 12:20:55 -0800

    shifted t0 assignment line to just before while loop

--- ros_workspaces/ourProj/src/our_pkg/src/turtlebot_controller.py
+++ ros_workspaces/ourProj/src/our_pkg/src/turtlebot_controller.py
@@ -39,7 +39,6 @@ def controller():
             rospy.loginfo("")
 
         if len(grids_traversed) == 5:
-            t0 = rospy.Time.now().to_sec()
             current_angle = 0
 
             vel_msg.linear.x = 0
@@ -48,7 +47,7 @@ def controller():
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
             vel_msg.angular.z = 1
-
+            t0 = rospy.Time.now().to_sec()
             while (current_angle < rot_angle):
                 pub.publish(vel_msg)
                 t1 = rospy.Time.now().to_sec()
